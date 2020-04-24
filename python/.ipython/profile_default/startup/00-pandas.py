58f843b8ecd85c90585b95e0cafec96bedbb0f6a
Torsten Sprenger <mail@spren9er.de>
2020-04-24 14:24:20 +0200

    Update initial pandas options

--- python/.ipython/profile_default/startup/00-pandas.py
+++ python/.ipython/profile_default/startup/00-pandas.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 pd.set_option('display.max_rows', 100)
-pd.set_option('precision', 5)
+pd.set_option('display.show_dimensions', False)
+pd.set_option('display.float_format', lambda x: f"{x:.2f}")
+pd.set_option('precision', 6)
