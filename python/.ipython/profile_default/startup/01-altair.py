36388047d93411925d10546d64cdfc37dcf08e5c
Torsten Sprenger <mail@spren9er.de>
2020-10-24 17:16:48 +0200

    Update settings of vs code and jupyter

--- python/.ipython/profile_default/startup/01-altair.py
+++ python/.ipython/profile_default/startup/01-altair.py
@@ -1,6 +1,7 @@
-import altair as alt
-import os
 import json
+import os
+
+import altair as alt
 
 dir = os.path.dirname(__file__)
 rel_path = "../../../../vega_lite/vl_theme_spren9er.json"
@@ -9,9 +10,10 @@ file_path = os.path.join(dir, rel_path)
 with open(file_path, "r") as json_file:
     vl_theme = json.load(json_file)
     alt.themes.register(
-        "spren9er", lambda: {"config": vl_theme, "width": 500, "height": 350},
+        "spren9er",
+        lambda: {"config": vl_theme, "width": 500, "height": 350},
     )
 
 alt.themes.enable("spren9er")
 alt.data_transformers.disable_max_rows()
-alt.renderers.set_embed_options(actions=False)
+alt.renderers.set_embed_options(actions=False, scaleFactor=2)
