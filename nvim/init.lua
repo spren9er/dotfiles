444f4978765cafd56414e4dd60628f9ebd407cfa
Torsten Sprenger <mail@spren9er.de>
2025-08-07 22:00:50 +0200

    Restructure neovim folder

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,18 +7,18 @@ vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
-require 'options'
+require 'config/options'
 
 -- [[ Basic Keymaps ]]
-require 'keymaps'
+require 'config/keymaps'
 
 -- [[ Basic Autocommands ]]
-require 'auto-commands'
+require 'config/autocmds'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+require 'config/lazy-bootstrap'
 
 -- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+require 'config/lazy-plugins'
 
 -- vim: ts=2 sts=2 sw=2 et
---
+++ nvim/lua/config/lazy-plugins.lua
@@ -0,0 +1,41 @@
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+
+---@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+-- [[ Load plugins via lazy.vim ]]
+require('lazy').setup({
+  spec = {
+    { import = 'plugins' },
+  },
+}, {
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      lazy = '💤 ',
+      plugin = '🔌',
+      require = '🌙',
+      runtime = '💻',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
