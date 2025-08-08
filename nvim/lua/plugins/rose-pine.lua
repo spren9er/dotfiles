df386718b92f7341c526d8756f68c312764c17f0
Torsten Sprenger <mail@spren9er.de>
2025-08-08 20:34:36 +0200

    Fix rose pine theme

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -3,8 +3,8 @@ return {
   name = 'rose-pine',
   config = function()
     require('rose-pine').setup {
-      variant = 'auto', -- auto, main, moon, or dawn
-      dark_variant = 'main', -- main, moon, or dawn
+      variant = 'moon', -- auto, main, moon, or dawn
+      dark_variant = 'moon', -- main, moon, or dawn
       dim_inactive_windows = false,
       extend_background_behind_borders = true,
 
