4c786c0f1f442bfd902dda1656af4c21a32fbd8d
Torsten Sprenger <mail@spren9er.de>
2025-08-24 21:41:22 +0200

    Remove shortcut for joining lines

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -41,9 +41,6 @@ vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Prev match' })
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
--- Keep cursor position while joining lines
-vim.keymap.set('n', 'J', 'mzJ`z')
-
 -- Keep cursor position while indenting
 vim.keymap.set('n', '=ap', "ma=ap'a")
 
